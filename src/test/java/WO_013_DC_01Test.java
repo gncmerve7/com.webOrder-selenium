6bb91bfe8660cea19733898248965708b14c1c46
Merve Genc <gncmerve7@gmail.com>
2024-01-09 13:51:03 -0500

    update WO_13DC_01

--- src/test/java/WO_013_DC_01Test.java
+++ src/test/java/WO_013_DC_01Test.java
@@ -16,53 +16,45 @@ import org.openqa.selenium.WebElement;
 
 public class WO_013_DC_01Test extends Hooks {
 
-	@Test
-	void completedOrderTotalDisplayFailiure() throws InterruptedException {
+    @Test
+    void completedOrderTotalDisplayFailiure() throws InterruptedException {
 
-		WebElement webOrderTab = driver.findElement(By.linkText("Weborder"));
-		webOrderTab.click();
-		Thread.sleep(2000);
+        WebElement webOrderTab = driver.findElement(By.linkText("Weborder"));
+        webOrderTab.click();
+        Thread.sleep(2000);
 
-		WebElement usernameInput = driver.findElement(By.id("login-username-input"));
-		usernameInput.sendKeys("Inar");
-		WebElement passwordInput = driver.findElement(By.id("login-password-input"));
-		passwordInput.sendKeys("Academy");
-		WebElement loginButton = driver.findElement(By.id("login-button"));
+        WebElement usernameInput = driver.findElement(By.id("login-username-input"));
+        usernameInput.sendKeys("Inar");
+        WebElement passwordInput = driver.findElement(By.id("login-password-input"));
+        passwordInput.sendKeys("Academy");
+        WebElement loginButton = driver.findElement(By.id("login-button"));
 
-		loginButton.click();
+        loginButton.click();
 
-		WebElement viewAllOrdersTab = driver.findElement(By.linkText("View all orders"));
-		viewAllOrdersTab.click();
-		WebElement addButton = driver.findElement(By.xpath("//button[contains(text(),'Add More Data')]"));
-		addButton.click();
+        WebElement viewAllOrdersTab = driver.findElement(By.linkText("View all orders"));
+        viewAllOrdersTab.click();
+        Thread.sleep(3000);
+        WebElement addButton = driver.findElement(By.xpath("//button[contains(text(),'Add More Data')]"));
+        addButton.click();
+        Thread.sleep(3000);
+        WebElement orderInformation = driver.findElement(By.xpath("//tbody/tr[1]/td[13]/a[1]/*[1]"));
+        orderInformation.click();
+        Thread.sleep(3000);
 
-		WebElement orderInformation = driver.findElement(By.xpath("//tbody/tr[1]/td[13]/a[1]/*[1]"));
-		orderInformation.click();
+
+        WebElement totalDisplayedBeforeCalculation = driver.findElement(By.id("totalInput"));
+        String totalBefore = totalDisplayedBeforeCalculation.getAttribute("value");
+        System.out.println(totalBefore);
 		Thread.sleep(3000);
 
-		WebElement totalDisplayedBeforeCalculation = driver.findElement(By.xpath("//input[@id='totalInput']"));
-		Thread.sleep(3000);
-		String totalBefore=totalDisplayedBeforeCalculation.getText();
-		System.out.println(totalBefore);
-totalDisplayedBeforeCalculation.getText();
-		WebElement calculateButton = driver.findElement(By.xpath("//button[contains(text(),'Calculate')]"));
+        WebElement calculateButton = driver.findElement(By.xpath("//button[contains(text(),'Calculate')]"));
 		calculateButton.click();
-		Thread.sleep(3000);
-		WebElement totalDisplayedAfterCalculation = driver.findElement(By.xpath("//input[@id='totalInput']"));
-		Thread.sleep(3000);
-		String totalAfter=totalDisplayedAfterCalculation.getText();
-		System.out.println(totalAfter);
-totalDisplayedAfterCalculation.getText();
+        Thread.sleep(3000);
 
-		Assertions.assertEquals(totalAfter,totalBefore);
-
-
-		//Assertions.assertNotEquals(totalAfter,totalBefore);
-		//Assertions.assertSame(totalDisplayedBeforeCalculation,totalDisplayedAfterCalculation);
-		//Assertions.assertFalse(totalDisplayedBeforeCalculation==totalDisplayedAfterCalculation);
-		//Assertions.assertNotSame(totalDisplayedBeforeCalculation.getText(),totalDisplayedAfterCalculation.getText());
-		//Assertions.assertNotEquals(totalDisplayedBeforeCalculation.toString(),totalDisplayedAfterCalculation.toString());
-		Thread.sleep(3000);
-		// ?
-	}
+        WebElement totalDisplayedAfterCalculation = driver.findElement(By.id("totalInput"));
+        Thread.sleep(3000);
+        String totalAfter =  totalDisplayedAfterCalculation.getAttribute("value"); // ***
+		Assertions.assertEquals(totalAfter, totalBefore);
+        Thread.sleep(3000);
+    }
 }
